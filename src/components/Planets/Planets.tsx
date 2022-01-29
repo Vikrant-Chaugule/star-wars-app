bc679011400a618dce59dcf41544fb7a2d44dd7a
Vikrant Chaugule <chaugulevikrant005@gmail.com>
2022-01-30 00:35:12 +0530

    added search for planets and starships

--- src/components/Planets/Planets.tsx
+++ src/components/Planets/Planets.tsx
@@ -1,5 +1,7 @@
 import { useQuery } from "@apollo/client";
+import { useEffect } from "react";
 import { GET_ALL_PLANETS } from "../../queries/planetsQueries";
+import { useSearchData } from "../Hooks/useSearchData";
 import { ListItem } from "../ListItem/ListItem";
 import { Loader } from "../Loader/Loader";
 import "./Planets.css";
@@ -7,28 +9,49 @@ import "./Planets.css";
 export const Planets = () => {
   const { error, loading, data } = useQuery(GET_ALL_PLANETS);
 
+  const { onSearch, filteredData, setData, searchValue, setFilteredData } =
+    useSearchData();
+
+  useEffect(() => {
+    const films = data?.allPlanets?.planets;
+    setFilteredData(films);
+    setData(films);
+  }, [data, setFilteredData, setData]);
+
   if (error) return <p>Error</p>;
 
   if (loading) return <Loader />;
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onSearch(e.target.value);
+  };
 
   return (
     <div className="planets">
       <div className="films-header">
         <h1>Planets</h1>
-        <input placeholder="Search Planets..." />
+        <input
+          placeholder="Search Planets..."
+          value={searchValue}
+          onChange={handleChange}
+        />
       </div>
 
       <div className="planets-list">
-        {data.allPlanets.planets.map(
-          ({ id, name, population }: any, index: number) => (
-            <ListItem
-              key={id}
-              id={id}
-              index={index}
-              title={name}
-              subTitle={`Population - ${population ?? "N/A"}`}
-            />
-          )
+        {filteredData && filteredData.length > 0 ? (
+          filteredData.map(({ id, name, population }: any, index: number) => {
+            return (
+              <ListItem
+                key={id}
+                id={id}
+                index={index}
+                title={name}
+                subTitle={`Population - ${population ?? "N/A"}`}
+              />
+            );
+          })
+        ) : (
+          <ListItem id="" index={-1} title="Planet Found" subTitle="" />
         )}
       </div>
     </div>
--- src/components/Starships/Starships.tsx
+++ src/components/Starships/Starships.tsx
@@ -1,5 +1,7 @@
 import { useQuery } from "@apollo/client";
+import { useEffect } from "react";
 import { GET_ALL_STARSHIPS } from "../../queries/starshipsQueries";
+import { useSearchData } from "../Hooks/useSearchData";
 import { ListItem } from "../ListItem/ListItem";
 import { Loader } from "../Loader/Loader";
 import "./Starships.css";
@@ -7,28 +9,49 @@ import "./Starships.css";
 export const Starships = () => {
   const { error, loading, data } = useQuery(GET_ALL_STARSHIPS);
 
+  const { onSearch, filteredData, setData, searchValue, setFilteredData } =
+    useSearchData();
+
+  useEffect(() => {
+    const films = data?.allStarships?.starships;
+    setFilteredData(films);
+    setData(films);
+  }, [data, setFilteredData, setData]);
+
   if (error) return <p>Error</p>;
 
   if (loading) return <Loader />;
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onSearch(e.target.value);
+  };
 
   return (
     <div className="starships">
       <div className="starships-header">
         <h1>Starships</h1>
-        <input placeholder="Search Starship..." />
+        <input
+          placeholder="Search Starship..."
+          value={searchValue}
+          onChange={handleChange}
+        />
       </div>
 
       <div className="starships-list">
-        {data.allStarships.starships.map(
-          ({ id, name, model }: any, index: number) => (
-            <ListItem
-              key={id}
-              id={id}
-              index={index}
-              title={name}
-              subTitle={`Model - ${model ?? "N/A"}`}
-            />
-          )
+        {filteredData && filteredData.length > 0 ? (
+          filteredData.map(({ id, name, population }: any, index: number) => {
+            return (
+              <ListItem
+                key={id}
+                id={id}
+                index={index}
+                title={name}
+                subTitle={`Population - ${population ?? "N/A"}`}
+              />
+            );
+          })
+        ) : (
+          <ListItem id="" index={-1} title="Starship Found" subTitle="" />
         )}
       </div>
     </div>
